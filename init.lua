16efbdf453ae6ab132f718e2be8c5bfa4080e477
Tobias Hunger <tobias.hunger@slint-ui.com>
2024-01-05 17:54:34 +0100

    Update winbar to v4 syntax

--- init.lua
+++ init.lua
@@ -47,7 +47,7 @@ require("lazy").setup {
       "L3MON4D3/LuaSnip",
       config = function(_, opts)
         require("luasnip").setup(opts)
-        require("luasnip.loaders.from_vscode").lazy_load { paths = { vim.fn.stdpath 'config' .. '/snippets' } } -- Not needed on windows!
+        require("luasnip.loaders.from_vscode").lazy_load { paths = { vim.fn.stdpath "config" .. "/snippets" } } -- Not needed on windows!
       end,
     },
     -- force enable nvim-dap on windows
@@ -76,80 +76,11 @@ require("lazy").setup {
           format_on_save = {
             -- enable or disable format on save globally
             enabled = false,
-            -- enable format on save for specified filetypes only
-            allow_filetypes = {
-              "rust",
-            },
           },
           -- default format timeout
           timeout_ms = 10000,
         },
       },
-    },
-    {
-      "hrsh7th/nvim-cmp",
-      opts = function(_, opts)
-        local cmp = require "cmp"
-        local compare = require "cmp.config.compare"
-        local luasnip = require "luasnip"
-
-        local function has_words_before()
-          local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-          return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
-        end
-
-        return require("astrocore").extend_tbl(opts, {
-          window = { completion = { col_offset = -1, side_padding = 0 } },
-          sorting = {
-            comparators = {
-              compare.offset,
-              compare.exact,
-              compare.score,
-              compare.recently_used,
-              function(entry1, entry2)
-                local _, entry1_under = entry1.completion_item.label:find "^_+"
-                local _, entry2_under = entry2.completion_item.label:find "^_+"
-                entry1_under = entry1_under or 0
-                entry2_under = entry2_under or 0
-                if entry1_under > entry2_under then
-                  return false
-                elseif entry1_under < entry2_under then
-                  return true
-                end
-              end,
-              compare.kind,
-              compare.sort_text,
-              compare.length,
-              compare.order,
-            },
-          },
-          mapping = {
-            -- tab complete
-            ["<Tab>"] = cmp.mapping(function(fallback)
-              if cmp.visible() and has_words_before() then
-                cmp.confirm { select = true }
-              else
-                fallback()
-              end
-            end, { "i", "s" }),
-            -- <C-n> and <C-p> for navigating snippets
-            ["<C-n>"] = cmp.mapping(function()
-              if luasnip.jumpable(1) then luasnip.jump(1) end
-            end, { "i", "s" }),
-            ["<C-p>"] = cmp.mapping(function()
-              if luasnip.jumpable(-1) then luasnip.jump(-1) end
-            end, { "i", "s" }),
-            -- <C-j> for starting completion
-            ["<C-j>"] = cmp.mapping(function()
-              if cmp.visible() then
-                cmp.select_next_item { behavior = cmp.SelectBehavior.Insert }
-              else
-                cmp.complete()
-              end
-            end, { "i", "s" }),
-          },
-        })
-      end,
     },
     {
       "nvim-telescope/telescope.nvim",
@@ -188,29 +119,77 @@ require("lazy").setup {
     -- override heirline config
     {
       "rebelot/heirline.nvim",
-      opts = function(_, opts)
-        local components = require "astroui.status.component"
+      -- Disable mappings for tabline
+      dependencies = { "AstroNvim/astrocore" },
+      -- Customize UI
+      opts = function(_, orig)
+        local status = require "astroui.status"
 
-        opts.statusline = {
-          hl = { fg = "fg", bg = "bg" },
-          components.mode(),
-          components.git_branch(),
-          components.git_diff(),
-          components.fill(),
-          components.lsp(),
-          components.treesitter(),
-          components.nav { scrollbar = false, percentage = false, padding = { left = 1 } },
-          components.mode { surround = { separator = "right" } },
+        return {
+          opts = orig.opts,
+          statusline = { -- statusline
+            hl = { fg = "fg", bg = "bg" },
+            status.component.mode(),
+            status.component.git_branch(),
+            status.component.fill(),
+            status.component.cmd_info(),
+            status.component.fill(),
+            status.component.lsp(),
+            status.component.virtual_env(),
+            status.component.treesitter(),
+            status.component.nav { scrollbar = false, percentage = false },
+            status.component.mode { surround = { separator = "right" } },
+          },
+          winbar = { -- winbar
+            init = function(self) self.bufnr = vim.api.nvim_get_current_buf() end,
+            fallthrough = false,
+            -- inactive winbar
+            {
+              condition = function() return not status.condition.is_active() end,
+              -- show the path to the file relative to the working directory
+              status.component.separated_path(),
+              -- add the file name and icon
+              status.component.file_info {
+                file_icon = { hl = status.hl.file_icon "winbar", padding = { left = 0 } },
+                filename = { modify = ":p:." },
+                filetype = false,
+                file_modified = false,
+                file_read_only = false,
+                hl = status.hl.get_attributes("winbarnc", true),
+                surround = false,
+                update = "BufEnter",
+              },
+            },
+            -- active winbar
+            {
+              -- show the path to the file relative to the working directory
+              status.component.separated_path(),
+              -- add the file name and icon
+              status.component.file_info { -- add file_info to breadcrumbs
+                file_icon = { hl = status.hl.filetype_color, padding = { left = 0 } },
+                filename = { modify = ":p:." },
+                filetype = false,
+                file_modified = false,
+                file_read_only = false,
+                hl = status.hl.get_attributes("winbar", true),
+                surround = false,
+                update = "BufEnter",
+              },
+              -- show the breadcrumbs
+              status.component.breadcrumbs {
+                icon = { hl = true },
+                hl = status.hl.get_attributes("winbar", true),
+                prefix = true,
+                padding = { left = 0 },
+              },
+              status.component.fill(),
+              status.component.git_diff(),
+              status.component.diagnostics(),
+            },
+          },
+          tabline = {},
+          statuscolumn = orig.statuscolumn,
         }
-        opts.winbar = {
-          hl = { fg = "fg", bg = "bg" },
-          components.file_info { filename = { modify = ":p:." } },
-          components.breadcrumbs { icon = { hl = true }, padding = { left = 1 } },
-          components.fill(),
-          components.diagnostics(),
-        }
-
-        return opts
       end,
     },
   },
